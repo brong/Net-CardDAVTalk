884068474657cf352aacaaa4cb4f00ec92d8780c
Bron Gondwana <brong@fastmail.fm>
2016-05-03 09:41:12 +1000

    update version

--- lib/Net/CardDAVTalk.pm
+++ lib/Net/CardDAVTalk.pm
@@ -21,11 +21,11 @@ Net::CardDAVTalk - A library for talking to CardDAV servers
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
