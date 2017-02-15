c4fab0a216e97a61445ef5921b117d92005f54b7
Bron Gondwana <brong@fastmail.fm>
2017-02-15 11:02:22 +1100

    Version 0.04, now with ACL support

--- lib/Net/CardDAVTalk.pm
+++ lib/Net/CardDAVTalk.pm
@@ -22,11 +22,11 @@ Net::CardDAVTalk - A library for talking to CardDAV servers
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
